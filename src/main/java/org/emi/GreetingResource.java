8fe0d840cc52f5b96c072927164b7a1bbb09676e
Emi Covreag <emi.covreag@gmail.com>
2026-04-23 18:16:23 +0300

    change endpoint and test

--- src/main/java/org/emi/GreetingResource.java
+++ src/main/java/org/emi/GreetingResource.java
@@ -2,19 +2,15 @@ package org.emi;
 
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
-import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.MediaType;
 
 @Path("/hello")
 public class GreetingResource {
 
     @GET
-    @Path("/{name}")
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello(@PathParam("name") String name) {
-        return "Hello " + name;
+    public String hello() {
+        return "Hello from Quarkus REST";
     }
 }
-
